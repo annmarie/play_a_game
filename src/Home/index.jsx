3f1575952f875a106ac6b046267717638a501d61
Annmarie Conway <annmarie.daylife@gmail.com>
2025-03-15 21:10:30 -0400

    updated homepage styles

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
+import './styles.css'
 
 const Hello = () => {
-  return (
+  return (<div className="main">
+    <h1 className="header">Play A Game</h1>
     <ul className="list">
       <li className="list-item"><Link to="/backgammon">Backgammon</Link></li>
       <li className="list-item"><Link to="/connect4">Connect4</Link></li>
       <li className="list-item"><Link to="/slidepuzzle">SlidePuzzle</Link></li>
       <li className="list-item"><Link to="/pegsolitaire">PegSolitaire</Link></li>
     </ul>
-  );
+  </div>);
 };
 
 export default Hello
