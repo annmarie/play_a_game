88ad8c9fdd165ee43ebba3d5d513d676745e0ed1
Annmarie Conway <annmarie.daylife@gmail.com>
2025-03-15 20:25:52 -0400

    added style to home page list

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,12 @@ import TicTacToe from './TicTacToe';
 import Backgammon from './Backgammon';
 
 const Hello = () => {
-
   return (
-    <ul>
-      <li><Link to="/backgammon">Backgammon</Link></li>
-      <li><Link to="/connect4">Connect4</Link></li>
-      <li><Link to="/slidepuzzle">SlidePuzzle</Link></li>
-      <li><Link to="/pegsolitaire">PegSolitaire</Link></li>
+    <ul className="list">
+      <li className="list-item"><Link to="/backgammon">Backgammon</Link></li>
+      <li className="list-item"><Link to="/connect4">Connect4</Link></li>
+      <li className="list-item"><Link to="/slidepuzzle">SlidePuzzle</Link></li>
+      <li className="list-item"><Link to="/pegsolitaire">PegSolitaire</Link></li>
     </ul>
   );
 };
