bf2d25146788a4255663178caccc1e8e9dd4f20c
Annmarie Conway <annmarie.daylife@gmail.com>
2025-11-21 23:49:18 -0500

    update layout

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 import './layout.css'
 
 const Header = () => {
   return (
     <div className="header">
-      <h1 >Play A Game</h1>
+      <h1><Link to="/">Play A Game</Link></h1>
     </div>
   );
 };
